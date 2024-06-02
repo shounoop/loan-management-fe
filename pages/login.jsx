ffabd3f8c6ff1b463ae8b98d1d12f2a576bc0f29
HMT <tuan.hm@tinasoft.com.vn>
2024-06-03 00:27:02 +0700

    update: login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { Button, Checkbox, Form, Grid, Input, theme, Typography } from 'antd';
 
 import { LockOutlined, MailOutlined } from '@ant-design/icons';
-import Axios from './../utils/axios';
+import Axios from '../utils/axios';
 import MyToast from '@mdrakibul8001/toastify';
 import { useRouter } from 'next/router';
 
@@ -16,7 +16,7 @@ export default function App() {
 	const router = useRouter();
 	const { http, saveToken, user } = Axios();
 
-	const [email, setEmail] = useState('');
+	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 
 	useEffect(() => {
@@ -29,19 +29,23 @@ export default function App() {
 	const screens = useBreakpoint();
 
 	const onFinish = async (values) => {
-		console.log(`${process.env.NEXT_PUBLIC_DOMAIN}/login`, values);
 		notify('info', 'Checking...!');
+
 		await http
-			.post(`${process.env.NEXT_PUBLIC_DOMAIN}/api/login`, { email, password })
+			.post(`${process.env.NEXT_PUBLIC_DOMAIN}/api/auth/login`, {
+				username,
+				password,
+			})
 			.then((res) => {
-				console.log(res.data.user);
-				const result = res.data?.user;
-				const user = {
-					id: result?.id,
-					name: result?.name,
-					email: result?.email,
-				};
-				saveToken(user, res.data.token);
+				console.log({ res });
+
+				// const result = res.data?.user;
+				// const user = {
+				// 	id: result?.id,
+				// 	name: result?.name,
+				// 	email: result?.email,
+				// };
+				// saveToken(user, res.data.token);
 			})
 			.catch((e) => {
 				const msg = e.response?.data;
@@ -56,14 +60,10 @@ export default function App() {
 			});
 
 		// remove these lines after api integration
-		saveToken(
-			{
-				id: 1,
-				name: 'Md Rakibul Islam',
-				email: 'fds@gmail.com',
-			},
-			'token'
-		);
+		// saveToken(
+		// 	{ id: 1, name: 'Md Rakibul Islam', email: 'fds@gmail.com' },
+		// 	'token'
+		// );
 	};
 
 	const styles = {
@@ -92,9 +92,7 @@ export default function App() {
 			height: screens.sm ? '100vh' : 'auto',
 			padding: screens.md ? `${token.sizeXXL}px 0px` : '0px',
 		},
-		text: {
-			color: token.colorTextSecondary,
-		},
+		text: { color: token.colorTextSecondary },
 		title: {
 			fontSize: screens.md ? token.fontSizeHeading2 : token.fontSizeHeading3,
 		},
@@ -104,31 +102,6 @@ export default function App() {
 		<section style={styles.section}>
 			<div style={styles.container}>
 				<div style={styles.header}>
-					<svg
-						width="25"
-						height="24"
-						viewBox="0 0 25 24"
-						fill="none"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<rect x="0.464294" width="24" height="24" rx="4.8" fill="#1890FF" />
-
-						<path
-							d="M14.8643 3.6001H20.8643V9.6001H14.8643V3.6001Z"
-							fill="white"
-						/>
-
-						<path
-							d="M10.0643 9.6001H14.8643V14.4001H10.0643V9.6001Z"
-							fill="white"
-						/>
-
-						<path
-							d="M4.06427 13.2001H11.2643V20.4001H4.06427V13.2001Z"
-							fill="white"
-						/>
-					</svg>
-
 					<Title style={styles.title}>Sign in</Title>
 
 					<Text style={styles.text}>
@@ -139,14 +112,28 @@ export default function App() {
 
 				<Form
 					name="normal_login"
-					initialValues={{
-						remember: true,
-					}}
+					initialValues={{ remember: true }}
 					onFinish={onFinish}
 					layout="vertical"
 					requiredMark="optional"
 				>
 					<Form.Item
+						name="username"
+						rules={[
+							{
+								required: true,
+								message: 'Please input your username!',
+							},
+						]}
+					>
+						<Input
+							prefix={<MailOutlined />}
+							placeholder="Username"
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+					</Form.Item>
+
+					{/* <Form.Item
 						name="email"
 						rules={[
 							{
@@ -161,16 +148,11 @@ export default function App() {
 							placeholder="Email"
 							onChange={(e) => setEmail(e.target.value)}
 						/>
-					</Form.Item>
+					</Form.Item> */}
 
 					<Form.Item
 						name="password"
-						rules={[
-							{
-								required: true,
-								message: 'Please input your Password!',
-							},
-						]}
+						rules={[{ required: true, message: 'Please input your Password!' }]}
 					>
 						<Input.Password
 							prefix={<LockOutlined />}
