472fb7280b5fa8e73b45cbb7ee8873a1689cbf4e
HMT <tuan.hm@tinasoft.com.vn>
2024-06-09 17:29:30 +0700

    feat(ui): loan methods screen

---
+++ components/HeadingWrapper/index.jsx
@@ -0,0 +1,22 @@
+import { Row, Col, Button } from 'antd';
+import styles from './Index.module.scss';
+
+const HeadingWrapper = (props) => {
+  const { title, onClickAddNew } = props;
+
+  return (
+    <div className={styles.wrapper}>
+      <Row justify="space-between" align="middle">
+        <Col className={styles.heading_title}>{title}</Col>
+
+        <Col>
+          <Button type="primary" size="middle" onClick={onClickAddNew}>
+            Add new
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default HeadingWrapper;
